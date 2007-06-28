529b7bb5a35cceef9d73da739c2925930cb31f7d
lexiongjia <lexiongjia@4b591cd5-a833-0410-8603-c1928dc92378>
2007-06-28 07:20:33 +0000

    add inject functions

--- trunk/ocass/console/ocac_wrk.cpp
+++ trunk/ocass/console/ocac_wrk.cpp
@@ -3,20 +3,66 @@
  */
 
 #include "liboch.h"
+#include "ca_cfg.h"
 #include "ocac_wrk.h"
 #include "ocac_misc.h"
 
-int OCAC_PWrkRun(OCACProc *pProc)
+static int OCAC_PCCWrk(OCACProc *pProc)
 {
+    CCWrkStateDesc wrkStateDesc;
+    CAErrno caErr;
+    CCWrk *pCWrk;
     int nProcExit = CA_PROC_EXIT_OK;
 
+    caErr = CC_Startup(&pCWrk);
+    if (CA_ERR_SUCCESS != caErr)
+    {
+        nProcExit = CA_PROC_EXIT_FATAL;
+        goto EXIT;
+    }    
+
+    CC_Touch(pCWrk);
+    for (;;)
+    {
+        caErr = CC_State(pCWrk, TRUE, &wrkStateDesc);
+        if (CA_ERR_SUCCESS != caErr)
+        {
+            continue;
+        }
+
+        Sleep(1000);
+        if (wrkStateDesc.bIsDirty)
+        {
+            printf("Steat %u\n", wrkStateDesc.wrkState);
+        }
+    }
+
+EXIT:
     return nProcExit;
 }
 
-int OCAC_PWrkStop(OCACProc *pProc)
+static int OCAC_PWrkRun(OCACProc *pProc)
+{
+    CAErrno caErr;
+    int nProcExit = CA_PROC_EXIT_OK;
+
+    caErr = CA_CfgSetRTFromFile(pProc->szProcCfgFName, pProc->szProcWrkPath);
+    if (CA_ERR_SUCCESS != caErr)
+    {
+        nProcExit = CA_PROC_EXIT_INIT_FAILED;
+        goto EXIT;
+    }    
+
+    OCAC_PCCWrk(pProc);
+EXIT:
+    return nProcExit;
+}
+
+static int OCAC_PWrkStop(OCACProc *pProc)
 {
     int nProcExit = CA_PROC_EXIT_OK;
 
+    /* XXX */
     return nProcExit;
 }
 
