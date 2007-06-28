83804164e2144159d46354e586a013e359e8a388
lexiongjia <lexiongjia@4b591cd5-a833-0410-8603-c1928dc92378>
2007-06-28 07:15:55 +0000

    add inject functions

---
+++ trunk/ocass/include/ca_mod.h
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+#ifndef _CA_MOD_H_
+#define _CA_MOD_H_ 1
+
+#define CA_MOD_OCA  (TEXT("liboca.dll"))
+#define CA_MOD_OCC  (TEXT("libocc.dll"))
+#define CA_MOD_OCH  (TEXT("liboch.dll"))
+#define CA_MOD_OCS  (TEXT("libocs.dll"))
+
+
+#endif /* !defined(_CA_MOD_H_) */
