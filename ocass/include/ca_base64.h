311ef255f66085ea854ff91ce567bd53477f5067
lexiongjia <lexiongjia@4b591cd5-a833-0410-8603-c1928dc92378>
2007-06-30 23:54:22 +0000

    add base64

--- ocass/include/ca_base64.h
+++ ocass/include/ca_base64.h
@@ -7,4 +7,16 @@
 
 #include "ca_types.h"
 
+CA_DECLARE(int) CA_Base64EncodeLen(int nLen);
+CA_DECLARE(int) CA_Base64DecodeLen(const char *pBufCoded);
+
+CA_DECLARE(int) CA_Base64Encode(char *pEncoded, const char *pszStr, int nLen);
+CA_DECLARE(int) CA_Base64Decode(char *pBufplain, const char *pBufCoded);
+
+CA_DECLARE(int) CA_Base64DecodeBin(unsigned char *pBufPlain, 
+                                   const char *pBufCoded);
+
+CA_DECLARE(int) CA_Base64EncodeBin(char *pEncoded, const unsigned char *pStr, 
+                                   int nLen);
+
 #endif /* !defined(_CA_BASE64_H_) */
