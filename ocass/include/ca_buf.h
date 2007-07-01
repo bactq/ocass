13bc914d18d90eeb83cad00c43cb8cbfbf9912e5
lexiongjia <lexiongjia@4b591cd5-a833-0410-8603-c1928dc92378>
2007-07-01 14:47:21 +0000

    add buf parse

--- ocass/include/ca_buf.h
+++ ocass/include/ca_buf.h
@@ -10,4 +10,7 @@
 CA_DECLARE(void) CA_BTruncateNCpy(const char *pSrcBuf, int nSrcBufLen, 
                                   char *pDestBuf, int *pnDestBufLen);
 
+CA_DECLARE(const char *) CA_BMatching(const char *pBStart, int nBSLen, 
+                                      const char *pBMatching, int nBMLen);
+
 #endif /* !defined(_CA_BUF_H_) */
